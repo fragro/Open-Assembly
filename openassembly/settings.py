7779ff1e434dfcb2ac35e3953fdf32d1294877d9
Frank Grove <fragro@gmail.com>
2011-12-02 15:42:12 -0600

    added ln to static dirs in settings

--- openassembly/settings.py
+++ openassembly/settings.py
@@ -67,10 +67,29 @@ INSTALLED_APPS = (
     'django_mongodb_engine'
 )
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/'
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = '/home/dotcloud/data/media/'
 
-STATIC_ROOT = os.getcwd() + '/static/'
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/home/dotcloud/data/static/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 MARKITUP_MEDIA_URL = '/static/'
 MARKITUP_FILTER = ('markdown.markdown', {'safe_mode': True, 'previewParserPath': '/markitup/preview/'})
@@ -80,8 +99,6 @@ MARKITUP_SKIN = 'markitup/skins/simple'
 JQUERY_URL = '/static/jquery-1.4.2.min.js'
 
 DOMAIN_NAME = 'www.openassembly.org'
-
-ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 ADMINS = (('Frank', 'fragro@gmail.com'),)
 
