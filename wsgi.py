d72fc14b7e7941828bd19105bc33f3437d284120
Frank Grove <fragro@gmail.com>
2011-12-01 19:05:22 -0600

    trying to get it working with dotcloud

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 import os, sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname('openassembly/')))))
+print sys.path
 
 from django.core.handlers.wsgi import WSGIHandler
 os.environ["DJANGO_SETTINGS_MODULE"] = "openassembly.settings"
