b59f222a27538acfbf547f73099ad3b29e4e2419
Frank Grove <fragro@gmail.com>
2011-12-01 20:57:05 -0600

    updating files to newest version

--- openassembly/pirate_social/models.py
+++ openassembly/pirate_social/models.py
@@ -18,28 +18,33 @@ locations, photo verification systems, etc.
 """
 Locations can correspond to users, forum_blobs, or other objects.
 """
+
+
 class Location(models.Model):
     city = models.TextField(max_length=500, null=True, blank=True)
     state = models.TextField(max_length=500, null=True, blank=True)
     country = models.TextField(max_length=500, null=True, blank=True)
-    object_pk = models.IntegerField(_('object ID'))
+    object_pk = models.CharField(_('object ID'), max_length=100)
     content_object = generic.GenericForeignKey(ct_field="content_type", fk_field="object_pk")
     content_type = models.ForeignKey(ContentType,
                                       verbose_name=_('content type'),
                                       related_name="content_type_set_for_%(class)s")
-    
+
+
 class Address(models.Model):
     address = models.CharField(max_length=500)
     zipcode = models.TextField(max_length=10)
+
 
 class Subscription(models.Model):
     subscriber =  models.ForeignKey(User, verbose_name=_('subscriber'), related_name=_('subscriber'))
     subscribee = models.ForeignKey(User, verbose_name=_('subscribee'), related_name=_('subscribee'))
     created_dt = models.DateTimeField(_('date/time subscription created'), auto_now_add=True)
-    
+
     def __str__(self):
         return str(self.subscriber.username) + ":" + str(self.subscribee.username)
-    
+
+
 class RelationshipEvent(models.Model):
     initiator = models.ForeignKey(User, verbose_name=_('initiator user'), related_name=_('initiator'))
     target = models.ForeignKey(User, verbose_name=_('target user'), related_name=_('target'), blank=True,null=True)
