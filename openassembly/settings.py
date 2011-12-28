eeeda5ee45f087182ee8343ed07d863acc1842e0
Frank Grove <fragro@gmail.com>
2011-12-28 15:09:24 -0600

    changed domain name to new test

--- openassembly/settings.py
+++ openassembly/settings.py
@@ -128,7 +128,7 @@ MARKITUP_SKIN = 'simple'
 
 JQUERY_URL = '/static/jquery-1.4.2.min.js'
 
-DOMAIN_NAME = 'http://openassemblystaging-fragro.dotcloud.com/'
+DOMAIN_NAME = 'http://openassemblyrev43-fragro.dotcloud.com/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
