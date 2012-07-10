9ee97e3d51e48a9b2f0f29138ec5cd73f813b608
Frank Grove <fragro@gmail.com>
2012-07-10 09:45:05 -0500

    herping and derping

--- openassembly/settings.py
+++ openassembly/settings.py
@@ -299,5 +299,5 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = 'openassemblycongresscritter@gmail.com'
-    EMAIL_HOST_PASSWORD = 'this is a password'
+    EMAIL_HOST_PASSWORD = env['EMAIL_PASSWORD']
     EMAIL_PORT = 587
