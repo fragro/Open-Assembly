f9c00ad07910fbd568bd8da4583b6288b9e306d5
fragro <fragro@fragro-computa.(none)>
2012-08-23 15:20:56 -0500

    adding a linky funciton to JS so we can urlize in chat

--- ver1_0/openassembly/static/js/openassembly1.0.1.js
+++ ver1_0/openassembly/static/js/openassembly1.0.1.js
@@ -831,3 +831,22 @@ function load_patch(obj_pk, patch){
   }, "json");
 
 }
+
+if(!String.linkify) {
+    String.prototype.linkify = function() {
+
+        // http://, https://, ftp://
+        var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+
+        // www. sans http:// or https://
+        var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+
+        // Email addresses
+        var emailAddressPattern = /\w+@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6})+/gim;
+
+        return this
+            .replace(urlPattern, '<a href="$&" target="_blank">$&</a>')
+            .replace(pseudoUrlPattern, '$1<a href="http://$2" target="_blank">$2</a>')
+            .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+    };
+}
