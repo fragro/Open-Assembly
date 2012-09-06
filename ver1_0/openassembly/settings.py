afd79cc850c2787ec81f5a0a7a5ad0d39c5bdc9c
fragro <fragro@fragro-computa.(none)>
2012-09-06 01:32:57 -0500

    debugging static and uploaded files issue

--- ver1_0/openassembly/settings.py
+++ ver1_0/openassembly/settings.py
@@ -50,7 +50,7 @@ try:
         os.path.join(PROJECT_ROOT, 'openassembly/static/'),
     )
 
-    MEDIA_ROOT = '/home/dotcloud/store/media/'
+    MEDIA_ROOT = '/home/dotcloud/data/media/'
     STATIC_ROOT = '/home/dotcloud/data/static/'
     MEDIA_URL = '/media/'
 
